4d9feb8ff525360bfc42fc5b4818e82ae5cfed96
Siddhant Tripathi <60621886+siddhantripathi@users.noreply.github.com>
2021-02-02 16:18:11 +0530

    Update document_scanner.py

--- document_scanner.py
+++ document_scanner.py
@@ -1,117 +1,84 @@
 import cv2
 import numpy as np
-
-
-widthImg=480
-heightImg =640
-
-
+import utlis
+ 
+ 
+########################################################################
+webCamFeed = True
+pathImage = "1.jpg"
 cap = cv2.VideoCapture(0)
-cap.set(10,150)
-
-
-def preProcessing(img):
-    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
-    imgCanny = cv2.Canny(imgBlur,200,200)
-    kernel = np.ones((5,5))
-    imgDial = cv2.dilate(imgCanny,kernel,iterations=2)
-    imgThres = cv2.erode(imgDial,kernel,iterations=1)
-    return imgThres
-
-def getContours(img):
-    biggest = np.array([])
-    maxArea = 0
-    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area>5000:
-            #cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(cnt,True)
-            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
-            if area >maxArea and len(approx) == 4:
-                biggest = approx
-                maxArea = area
-    cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
-    return biggest
-
-def reorder (myPoints):
-    myPoints = myPoints.reshape((4,2))
-    myPointsNew = np.zeros((4,1,2),np.int32)
-    add = myPoints.sum(1)
-    #print("add", add)
-    myPointsNew[0] = myPoints[np.argmin(add)]
-    myPointsNew[3] = myPoints[np.argmax(add)]
-    diff = np.diff(myPoints,axis=1)
-    myPointsNew[1]= myPoints[np.argmin(diff)]
-    myPointsNew[2] = myPoints[np.argmax(diff)]
-    #print("NewPoints",myPointsNew)
-    return myPointsNew
-
-def getWarp(img,biggest):
-    biggest = reorder(biggest)
-    pts1 = np.float32(biggest)
-    pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])
-    matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
-
-    imgCropped = imgOutput[20:imgOutput.shape[0]-20,20:imgOutput.shape[1]-20]
-    imgCropped = cv2.resize(imgCropped,(widthImg,heightImg))
-
-    return imgCropped
-
-
-def stackImages(scale,imgArray):
-    rows = len(imgArray)
-    cols = len(imgArray[0])
-    rowsAvailable = isinstance(imgArray[0], list)
-    width = imgArray[0][0].shape[1]
-    height = imgArray[0][0].shape[0]
-    if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-                else:
-                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
-        ver = np.vstack(hor)
-    else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
-        ver = hor
-    return ver
-
+cap.set(10,160)
+heightImg = 640
+widthImg  = 480
+########################################################################
+ 
+utlis.initializeTrackbars()
+count=0
+ 
 while True:
-    success, img = cap.read()
-    img = cv2.resize(img,(widthImg,heightImg))
-    imgContour = img.copy()
-
-    imgThres = preProcessing(img)
-    biggest = getContours(imgThres)
-    if biggest.size !=0:
-        imgWarped=getWarp(img,biggest)
-        # imageArray = ([img,imgThres],
-        #           [imgContour,imgWarped])
-        imageArray = ([imgContour, imgWarped])
-        cv2.imshow("ImageWarped", imgWarped)
+ 
+    if webCamFeed:success, img = cap.read()
+    else:img = cv2.imread(pathImage)
+    img = cv2.resize(img, (widthImg, heightImg)) # RESIZE IMAGE
+    imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8) # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # CONVERT IMAGE TO GRAY SCALE
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1) # ADD GAUSSIAN BLUR
+    thres=utlis.valTrackbars() # GET TRACK BAR VALUES FOR THRESHOLDS
+    imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1]) # APPLY CANNY BLUR
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2) # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+ 
+    ## FIND ALL COUNTOURS
+    imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # FIND ALL CONTOURS
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10) # DRAW ALL DETECTED CONTOURS
+ 
+ 
+    # FIND THE BIGGEST COUNTOUR
+    biggest, maxArea = utlis.biggestContour(contours) # FIND THE BIGGEST CONTOUR
+    if biggest.size != 0:
+        biggest=utlis.reorder(biggest)
+        cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20) # DRAW THE BIGGEST CONTOUR
+        imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
+        pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
+        pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]]) # PREPARE POINTS FOR WARP
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+ 
+        #REMOVE 20 PIXELS FORM EACH SIDE
+        imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+        imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
+ 
+        # APPLY ADAPTIVE THRESHOLD
+        imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
+        imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
+        imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
+        imgAdaptiveThre=cv2.medianBlur(imgAdaptiveThre,3)
+ 
+        # Image Array for Display
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
+                      [imgBigContour,imgWarpColored, imgWarpGray,imgAdaptiveThre])
+ 
     else:
-        # imageArray = ([img, imgThres],
-        #               [img, img])
-        imageArray = ([imgContour, img])
-
-    stackedImages = stackImages(0.6,imageArray)
-    cv2.imshow("WorkFlow", stackedImages)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        imageArray = ([img,imgGray,imgThreshold,imgContours],
+                      [imgBlank, imgBlank, imgBlank, imgBlank])
+ 
+    # LABELS FOR DISPLAY
+    lables = [["Original","Gray","Threshold","Contours"],
+              ["Biggest Contour","Warp Prespective","Warp Gray","Adaptive Threshold"]]
+ 
+    stackedImage = utlis.stackImages(imageArray,0.75,lables)
+    cv2.imshow("Result",stackedImage)
+ 
+    # SAVE IMAGE WHEN 's' key is pressed
+    if cv2.waitKey(1) &amp; 0xFF == ord('s'):
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                      (1100, 350), (0, 255, 0), cv2.FILLED)
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Result', stackedImage)
+        cv2.waitKey(300)
+        count += 1
